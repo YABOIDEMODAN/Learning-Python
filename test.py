14a64442bd64120273acd6df64fb96d5e9abf635
YABOIDEMODAN <50401935+YABOIDEMODAN@users.noreply.github.com>
2019-05-09 13:41:23 -0400

    Defining a function
    Defining a function, adding variables, and setting up if-else statements

---
+++ test.py
@@ -0,0 +1,13 @@
+# this code does nothing for the program, it sets up a a basic "Hello World" function.
+def HW_Function():
+    print("Hellow World!")
+    print("Now I must go and learn more code")
+
+
+name = " Daniel Carmichael"
+nameInput = " Daniel Carmichael"
+if nameInput == name:
+
+    print("Hello" + name)
+else:
+    print('Goodbye')
