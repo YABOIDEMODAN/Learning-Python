3d1efa5dc063e3d6a13cf2b2e2664227c8ae8e2b
YABOIDEMODAN <50401935+YABOIDEMODAN@users.noreply.github.com>
2019-05-09 13:45:08 -0400

    Update test.py

--- test.py
+++ test.py
@@ -2,12 +2,17 @@
 def HW_Function():
     print("Hellow World!")
     print("Now I must go and learn more code")
+#___________________________________________________
 
-
+# setting up two global variables, name and nameInput, that will be use later in a section of code
 name = " Daniel Carmichael"
 nameInput = " Daniel Carmichael"
+
+# using an "if" statement to decide. if the code i want to equate is equal to one another
+# I use a comparisson operator (==) to determine if the global variables are the same
 if nameInput == name:
 
+# using an "else" statement to run a block of code in the instance that the if statement returns false
     print("Hello" + name)
 else:
     print('Goodbye')
