f9b47714654ee26578c4a308df60b4f600b4792c
YABOIDEMODAN <50401935+YABOIDEMODAN@users.noreply.github.com>
2019-05-09 17:08:33 -0400

    Update test.py
    This code will help you understand how to set up a function, a variable, an if/if-else statement, and an input. These are four basic snippets of code that are used in app development, website creation, and many more code implementations. Practice this code section and try to master it. Have fun, study, hard, and keep up the inspired work.

--- test.py
+++ test.py
@@ -6,13 +6,30 @@ def HW_Function():
 
 # setting up two global variables, name and nameInput, that will be use later in a section of code
 name = " Daniel Carmichael"
-nameInput = " Daniel Carmichael"
+# this code asks for a name input, "What is your name?:", down in the console. You will type your name in the console 
+# and if that name is equal to the name var then the console will print Hello + your name or a name you choose.
+nameInput = input("What is your name?: ")
 
-# using an "if" statement to decide. if the code i want to equate is equal to one another
-# I use a comparisson operator (==) to determine if the global variables are the same
+# using an "if" statement to decide. if the code i want to compare is equal to one another
+# I use a comparisson operand (==) to determine if the global variables are the same
 if nameInput == name:
 
 # using an "else" statement to run a block of code in the instance that the if statement returns false
     print("Hello" + name)
 else:
     print('Goodbye')
+
+    
+    # the condensed code looks like this
+def HW_Function():
+print("Hellow World!")
+print("Now I must go and learn more code")
+#___________________________________________________
+
+name = " Daniel Carmichael"
+nameInput = input("What is your name?: ")
+if nameInput == name:
+    print("Hello" + name)
+else:
+    print('Goodbye')
+# it is pretty simple, give it a try
